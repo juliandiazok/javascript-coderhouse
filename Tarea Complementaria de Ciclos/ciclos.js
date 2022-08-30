1a758f6187c07d0152d7f927ad20d54433eca5a2
juliandiazok <juliandiazok@hotmail.com>
2022-08-30 10:25:57 -0300

    tarea complementaria de ciclos

---
+++ Tarea Complementaria de Ciclos/ciclos.js
@@ -0,0 +1,9 @@
+/* CODIGO DEL CICLO */
+
+for (let i = 0; i <= 20; i++) {
+	if (i % 2 == 0) {
+		console.log('El numero ', i, ' es par campeón');
+	} else {
+		console.log('El numero ', i, ' es impar campeón');
+	}
+}
