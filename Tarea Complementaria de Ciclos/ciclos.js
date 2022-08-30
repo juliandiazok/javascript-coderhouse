a6cd72e0c180431953bd1343671d5858473cc92d
juliandiazok <juliandiazok@hotmail.com>
2022-08-30 10:31:21 -0300

    min changes

--- Tarea Complementaria de Ciclos/ciclos.js
+++ Tarea Complementaria de Ciclos/ciclos.js
@@ -1,6 +1,12 @@
 /* CODIGO DEL CICLO */
 
-for (let i = 0; i <= 20; i++) {
+let numFinal = parseInt(
+	prompt(
+		'Ingresar Numero, se calculará de 0 hasta dicho número si es par o impar'
+	)
+);
+
+for (let i = 0; i <= numFinal; i++) {
 	if (i % 2 == 0) {
 		console.log('El numero ', i, ' es par campeón');
 	} else {
