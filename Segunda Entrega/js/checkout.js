45200d7c3d3ed88aae1b5a046e09a31349b20d81
juliandiazok <juliandiazok@hotmail.com>
2022-10-19 17:40:11 -0300

    formulario de tarjeta agregado

--- Segunda Entrega/js/checkout.js
+++ Segunda Entrega/js/checkout.js
@@ -2,14 +2,24 @@ let storage = JSON.parse(localStorage.getItem('carrito'));
 
 const contenedorCarrito = document.getElementById('listaCarrito');
 
+let total = 0;
+
 storage.forEach((p) => {
 	const listaCarrito = document.createElement('div');
+	total += p.precio * p.cantidad;
 	listaCarrito.innerHTML = `
-                            <ul class="list-group list-group-horizontal-lg">
-                              <li class="list-group-item">${p.nombre}</li>
-                              <li class="list-group-item">$${p.precio}</li>
-                              <li class="list-group-item">${p.cantidad}</li>
-                            </ul>
+                              <li class="list-group-item d-flex justify-content-between align-items-center">
+                                ${p.nombre}   |   ${
+		p.marca
+	}   |   Precio Unitario: $${p.precio}   |   Total: $${p.precio * p.cantidad}
+                                <span class="badge bg-primary rounded-pill" style="background-color: #f48c06!important;">${
+																	p.cantidad
+																}</span>
+                              </li>
                           `;
 	contenedorCarrito.appendChild(listaCarrito);
 });
+
+totalCompra.innerHTML = total;
+
+console.log(total);
